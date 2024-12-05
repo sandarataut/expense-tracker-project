8359784de5a535af67c90c0d7957d47360c1a123
sandarminaye <sandarminaye@gmail.com>
2024-12-06 08:29:19 +1300

    - built ExpenseForm component with react hook form, zod validation library

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,14 @@ function App() {
           <h1>Expenses Tracker</h1>
         </div>
         <div className="mb-5">
-          <ExpenseForm />
+          <ExpenseForm
+            onSubmit={(expense) =>
+              setExpenses([
+                ...expenses,
+                { ...expense, id: expenses.length + 1 },
+              ])
+            }
+          />
         </div>
         <div className="mb-3">
           <ExpenseFilter
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,32 +1,100 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import categories from "../categories";
 
-const ExpenseForm = () => {
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters." })
+    .max(50),
+  amount: z
+    .number({ invalid_type_error: "Amount is required." })
+    .min(0.01)
+    .max(100_000),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: "Category is required." }),
+  }),
+});
+
+type ExpenseFormData = z.infer<typeof schema>;
+
+// interface FormData {
+//   description: string;
+//   amount: number;
+//   category: string;
+// }
+
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
+
+const ExpenseForm = ({ onSubmit }: Props) => {
+  //   console.log(useForm());
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    //   } = useForm<FormData>();
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+
+  //   const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <form>
+    <form
+      onSubmit={
+        // handleSubmit(onSubmit)
+        handleSubmit((data) => {
+          onSubmit(data);
+          reset();
+        })
+      }
+    >
       {/* .mb3>label.form-label+input.form-control */}
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input id="description" type="text" className="form-control" />
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       {/* .mb-3>label.form-label+input[type=number].form-control */}
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input id="amount" type="number" className="form-control" />
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       {/* .mb-3>label.form-label+select.form-select */}
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
           Category
         </label>
-        <select id="category" className="form-select">
+        <select {...register("category")} id="category" className="form-select">
           {categories.map((category) => (
-            <option key={category}>category</option>
+            <option key={category}>{category}</option>
           ))}
         </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
       {/* button.btn.btn-primary */}
       <button className="btn btn-primary">Submit</button>
